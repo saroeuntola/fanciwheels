4f309d20c938f66d53c976b6cf77b28fb77f6b9d
tola <tolasweatboy@gmail.com>
2025-08-07 21:02:10 +0700

    fix code

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -98,6 +97,7 @@
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
             gap: 3rem;
+            padding: 60px 0px;
         }
 
         .footer-section {
