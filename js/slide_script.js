1d225c8b8cd22b546adf5e853125a8ea0e8f9890
tola <tolasweatboy@gmail.com>
2025-08-11 11:31:09 +0700

    update

--- js/slide_script.js
+++ js/slide_script.js
@@ -70,7 +70,7 @@
 
   // Auto slide
   function startAutoSlide() {
-    slideInterval = setInterval(nextSlide, 4000);
+    slideInterval = setInterval(nextSlide, 2000);
   }
 
   function stopAutoSlide() {
