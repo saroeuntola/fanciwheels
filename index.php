2fefeda3ee9f44084c96f19fbcd83f38a73203fa
tola <tolasweatboy@gmail.com>
2025-08-18 16:24:39 +0700

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include './admin/page/library/db.php';
-$lang = isset($_GET['lang']) && in_array($_GET['lang'], ['en', 'bn']) ? $_GET['lang'] : 'en';
+$lang = isset($_GET['lang']) && in_array($_GET['lang'], ['en', 'bn']) ? $_GET['lang'] : 'bn';
 ?>
 <!DOCTYPE html>
 <html lang="en">
