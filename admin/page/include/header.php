dc92ef177becba52a97c7d8c720c8c658608fb1c
tola <tolasweatboy@gmail.com>
2025-08-19 21:19:48 +0700

    update

--- admin/page/include/header.php
+++ admin/page/include/header.php
@@ -17,7 +17,7 @@ $user = $userLib->getUser($userId);
             <button class="sidebar-toggle" onclick="toggleSidebar()">☰</button>
             <div class="logo">
                 <span style="font-size: 28px;">⚡</span>
-                Admin
+                FancyWheel Admin System
             </div>
         </div>
         
@@ -27,27 +27,33 @@ $user = $userLib->getUser($userId);
                 <span class="search-icon">🔍</span>
             </div>
      
-            <div class="user-menu">
-    <a href="<?php echo $baseURL; ?>/admin/page/user/profile.php">
-    <?php if (!empty($user['profile'])): ?>
-        <img src="<?php echo $baseURL; ?>/admin/page/user/user_image/<?php echo htmlspecialchars($user['profile']); ?>"
-             alt="Avatar"
-             />
-    <?php else: ?>
-        <div class="">
-            <svg class="" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
-            </svg>
-        </div>
-    <?php endif; ?>
-    
-    <div>
-        <h6 class="text-sm font-semibold text-white">
-            <?php echo htmlspecialchars($_SESSION['username'] ?? 'User'); ?>
-        </h6>
-    </div>
-</a>
+     <div class="relative inline-block text-left">
+    <a href="<?= $baseURL; ?>/admin/page/user/profile.php" class="flex items-center space-x-3 p-2 rounded-lg hover:bg-gray-700 transition">
+        
+        <!-- Avatar -->
+        <?php if (!empty($user['profile'])): ?>
+            <img 
+                src="<?= $baseURL; ?>/admin/page/user/user_image/<?= htmlspecialchars($user['profile']); ?>" 
+                alt="Avatar" 
+                class="w-10 h-10 rounded-full object-cover border-2 border-white"
+            />
+        <?php else: ?>
+            <div class="w-10 h-10 rounded-full bg-gray-500 flex items-center justify-center text-white">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
+                </svg>
             </div>
+        <?php endif; ?>
+
+        <!-- Username -->
+        <div class="flex flex-col">
+            <h6 class="text-sm font-semibold text-white">
+                <?= htmlspecialchars($_SESSION['username'] ?? 'User'); ?>
+            </h6>
+        </div>
+    </a>
+</div>
+
 
             
         </div>
