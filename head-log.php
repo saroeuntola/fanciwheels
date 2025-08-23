56648835097a2e52ca842e67c8c74dbefaf08ab1
tola <tolasweatboy@gmail.com>
2025-08-23 20:49:41 +0700

    update

--- head-log.php
+++ head-log.php
@@ -1,14 +1,24 @@
 <?php
+// Path to logs folder
+$csv_folder = './log/';
+if (!is_dir($csv_folder)) mkdir($csv_folder, 0755, true);
 
-$csv_file = './logs/visitors.csv'; 
+$csv_file = $csv_folder . 'visitors.csv';
+
+// Create file with header if it doesn't exist
+if (!file_exists($csv_file)) {
+    file_put_contents($csv_file, "Date,IP,Page,Referrer,User Agent\n");
+}
+
+// Get visitor info
 $ip = $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
 $uri = $_SERVER['REQUEST_URI'] ?? '';
 $referrer = $_SERVER['HTTP_REFERER'] ?? 'Direct';
 $user_agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
 $datetime = date("Y-m-d H:i:s");
 
+// Append to CSV
 $fields = [$datetime, $ip, $uri, $referrer, $user_agent];
-
 $fp = fopen($csv_file, 'a');
 if ($fp) {
     fputcsv($fp, $fields);
