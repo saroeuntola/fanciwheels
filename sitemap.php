c692c00d90fff4e1f419676a7ec6a0dbf12e3257
tola <tolasweatboy@gmail.com>
2025-08-30 20:50:08 +0700

    update

--- sitemap.php
+++ sitemap.php
@@ -1,29 +1,50 @@
 <?php
 header("Content-Type: application/xml; charset=UTF-8");
 
+// Define pages with multilingual versions
 $pages = [
     [
+        'loc' => 'https://fanciwheel.com/detail?id=15',
+        'langs' => [
+            'en' => 'https://fanciwheel.com/detail?id=15&lang=en',
+            'bn' => 'https://fanciwheel.com/detail?id=15&lang=bn'
+        ],
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?id=11',
+        'langs' => [
+            'en' => 'https://fanciwheel.com/detail?id=11&lang=en',
+            'bn' => 'https://fanciwheel.com/detail?id=11&lang=bn'
+        ],
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
         'loc' => 'https://fanciwheel.com/',
+        'langs' => [
+            'en' => 'https://fanciwheel.com/?lang=en',
+            'bn' => 'https://fanciwheel.com/?lang=bn'
+        ],
         'changefreq' => 'weekly',
         'priority' => '1.0'
     ],
     [
         'loc' => 'https://fanciwheel.com/about',
+        'langs' => [
+            'en' => 'https://fanciwheel.com/about?lang=en',
+            'bn' => 'https://fanciwheel.com/about?lang=bn'
+        ],
         'changefreq' => 'weekly',
         'priority' => '0.8'
     ],
     [
         'loc' => 'https://fanciwheel.com/services',
-        'changefreq' => 'weekly',
-        'priority' => '0.8'
-    ],
-    [
-        'loc' => 'https://fanciwheel.com/detail?id=15&lang=en',
-        'changefreq' => 'weekly',
-        'priority' => '0.8'
-    ],
-    [
-        'loc' => 'https://fanciwheel.com/detail?id=11&lang=en',
+        'langs' => [
+            'en' => 'https://fanciwheel.com/services?lang=en',
+            'bn' => 'https://fanciwheel.com/services?lang=bn'
+        ],
         'changefreq' => 'weekly',
         'priority' => '0.8'
     ],
@@ -31,13 +52,18 @@ $pages = [
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="https://www.w3.org/1999/xhtml">
     <?php foreach ($pages as $page): ?>
         <url>
             <loc><?= htmlspecialchars($page['loc']) ?></loc>
             <lastmod><?= date('Y-m-d') ?></lastmod>
             <changefreq><?= $page['changefreq'] ?></changefreq>
             <priority><?= $page['priority'] ?></priority>
+
+            <?php foreach ($page['langs'] as $lang => $url): ?>
+                <xhtml:link rel="alternate" hreflang="<?= $lang ?>" href="<?= htmlspecialchars($url) ?>" />
+            <?php endforeach; ?>
         </url>
     <?php endforeach; ?>
 </urlset>
