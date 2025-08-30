986c1eb85977631c01506303033b7fec66234a6b
tola <tolasweatboy@gmail.com>
2025-08-30 14:27:12 +0700

    update

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $lang = isset($_GET['lang']) && in_array($_GET['lang'], ['en', 'bn']) ? $_GET['l
   <meta name="robots" content="index, follow">
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="google-site-verification" content="KQ_ffol2MIoJcfrqSEKOfToxbgsEPcFj3STGCvUen5U"/>
   <?php include 'head-log.php' ?>
   <?php
   include 'header.php'
