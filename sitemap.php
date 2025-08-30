bc70c734275bc54906ce7afc64f31db30841d987
tola <tolasweatboy@gmail.com>
2025-08-30 14:53:45 +0700

    UPDATE

--- sitemap.php
+++ sitemap.php
@@ -1,26 +1,43 @@
 <?php
 header("Content-Type: application/xml; charset=UTF-8");
+
+$pages = [
+    [
+        'loc' => 'https://fanciwheel.com/',
+        'changefreq' => 'weekly',
+        'priority' => '1.0'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/about',
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/services',
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?id=15&lang=en',
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?id=11&lang=en',
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+];
+
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-    <!-- Homepage -->
-    <url>
-        <loc>https://fanciwheel.com/</loc>
-        <lastmod><?= date('Y-m-d') ?></lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>https://fanciwheel.com/service</loc>
-        <lastmod><?= date('Y-m-d') ?></lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-
-    <url>
-        <loc>https://fanciwheel.com/about</loc>
-        <lastmod><?= date('Y-m-d') ?></lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
+    <?php foreach ($pages as $page): ?>
+        <url>
+            <loc><?= htmlspecialchars($page['loc']) ?></loc>
+            <lastmod><?= date('Y-m-d') ?></lastmod>
+            <changefreq><?= $page['changefreq'] ?></changefreq>
+            <priority><?= $page['priority'] ?></priority>
+        </url>
+    <?php endforeach; ?>
 </urlset>
