7c8a91583a8eb42988800a1b0a1a99843c7d4252
tola <tolasweatboy@gmail.com>
2025-09-02 15:06:03 +0700

    update

--- admin/page/category/create.php
+++ admin/page/category/create.php
@@ -1,19 +1,19 @@
 <?php
-include('../library/announcement_lib.php');
+include('../library/category_lib.php');
 include('../library/checkroles.php');
 protectPathAccess();
 
-$announcement = new Announcement();
+$cat = new Category();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $message = $_POST['message'];
-    $link = !empty($_POST['link']) ? $_POST['link'] : null;
+    $name = $_POST['name'];
+    $image = $_POST['cat_image'] ?? NULL;
 
-    if ($announcement->createAnnouncement($message, $link)) {
+    if ($cat->createCategory($name,$image)) {
         header("Location: index.php");
         exit;
     } else {
-        echo "<p class='text-red-500 text-center'>❌ Error: Announcement could not be created.</p>";
+        echo "<p class='text-red-500 text-center'>❌ Error: could not be created.</p>";
     }
 }
 ?>
@@ -29,33 +29,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
 
   <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-    <h2 class="text-2xl font-bold text-center mb-6">Create Announcement</h2>
+    <h2 class="text-2xl font-bold text-center mb-6">Create Category</h2>
 
     <!-- Announcement Form -->
     <form action="create" method="POST">
       
       <!-- Message -->
       <div class="mb-4">
-        <label class="block text-sm font-medium text-gray-700">Message</label>
-        <textarea name="message" required rows="4"
+        <label class="block text-sm font-medium text-gray-700">Category Name</label>
+        <input name="name" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
-                 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></input>
       </div>
 
-      <!-- Link -->
-      <div class="mb-4">
-        <label class="block text-sm font-medium text-gray-700">Link</label>
-        <input type="url" name="link" placeholder="https://example.com"
-          class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
-                 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-      </div>
+      
 
       <!-- Submit -->
       <div>
         <button type="submit"
           class="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-lg 
                  hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500">
-          Create Announcement
+          Create 
         </button>
       </div>
     </form>
