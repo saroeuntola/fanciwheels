acfa3d1f258eb8a2a3b71bce71e65fccee8aeff2
tola <tolasweatboy@gmail.com>
2025-09-18 14:08:29 +0700

    update

--- sitemap.php
+++ sitemap.php
@@ -4,28 +4,31 @@ header("Content-Type: application/xml; charset=UTF-8");
 $staticPages = [
     [
         'loc' => 'https://fanciwheel.com/',
-        'langs' => [
-            'en' => 'https://fanciwheel.com/?lang=en',
-            'bn' => 'https://fanciwheel.com/?lang=bn'
-        ],
+        'changefreq' => 'weekly',
+        'priority' => '1.0'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/index?lang=en',
+        'changefreq' => 'weekly',
+        'priority' => '1.0'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/about?lang=en',
         'changefreq' => 'weekly',
         'priority' => '1.0'
     ],
     [
         'loc' => 'https://fanciwheel.com/about',
-        'langs' => [
-            'en' => 'https://fanciwheel.com/about?lang=en',
-            'bn' => 'https://fanciwheel.com/about?lang=bn'
-        ],
         'changefreq' => 'weekly',
         'priority' => '0.8'
     ],
     [
         'loc' => 'https://fanciwheel.com/services',
-        'langs' => [
-            'en' => 'https://fanciwheel.com/services?lang=en',
-            'bn' => 'https://fanciwheel.com/services?lang=bn'
-        ],
+        'changefreq' => 'weekly',
+        'priority' => '0.8'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/services?lang=en',
         'changefreq' => 'weekly',
         'priority' => '0.8'
     ],
@@ -33,18 +36,22 @@ $staticPages = [
 
 $games = [
     [
-        'slug' => 'dhaka',
-        'langs' => [
-            'en' => 'https://fanciwheel.com/detail?slug=dhaka&lang=en',
-            'bn' => 'https://fanciwheel.com/detail?slug=dhaka&lang=bn'
-        ]
+        'loc' => 'https://fanciwheel.com/detail?slug=dhaka-to-chittagong-bus-counter&lang=en'
     ],
     [
-        'slug' => 'sundarbans',
-        'langs' => [
-            'en' => 'https://fanciwheel.com/detail?slug=sundarbans&lang=en',
-            'bn' => 'https://fanciwheel.com/detail?slug=sundarbans&lang=bn'
-        ]
+        'loc' => 'https://fanciwheel.com/detail?slug=how-to-travel-from-chittagong-to-dhaka&lang=en'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?slug=dhaka-to-chittagong-bus-counter&lang=bn'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?lang=bn&slug=how-to-travel-from-chittagong-to-dhaka'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?lang=en&slug=how-to-make-bangladeshi-milk-tea-5-steps-guide'
+    ],
+    [
+        'loc' => 'https://fanciwheel.com/detail?slug=how-to-make-bangladeshi-milk-tea-5-steps-guide&lang=bn'
     ],
 ];
 
@@ -53,19 +60,13 @@ $allPages = array_merge($staticPages, $games);
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($allPages as $page): ?>
         <url>
-            <loc><?= htmlspecialchars(current($page['langs'])) ?></loc>
+            <loc><?= htmlspecialchars($page['loc']) ?></loc>
             <lastmod><?= date('Y-m-d') ?></lastmod>
             <changefreq><?= $page['changefreq'] ?? 'weekly' ?></changefreq>
             <priority><?= $page['priority'] ?? '0.8' ?></priority>
-
-            <?php foreach ($page['langs'] as $lang => $url): ?>
-                <!-- Self-referencing and alternate language -->
-                <xhtml:link rel="alternate" hreflang="<?= $lang ?>" href="<?= htmlspecialchars($url) ?>" />
-            <?php endforeach; ?>
         </url>
     <?php endforeach; ?>
 </urlset>
